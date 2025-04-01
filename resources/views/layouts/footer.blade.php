6e142a320cf24729ff14778ff16fc6ae73d940ce
nicolas rouillé <nicolas.rouillelanoe@gmail.com>
2025-04-01 21:50:40 +0200

    glpi link footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,4 @@
 <footer class="bg-dark text-white text-center py-3 mt-5">
+<p><a href="http://90.121.52.205:50293/" target="_blank"><button>Un probleme ? Connectez vous a GLPI pour créer un ticket.</button></a></p>
     © {{ date('Y') }} Numérathèque - Tous droits réservés.
 </footer>
